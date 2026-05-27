0b005446fcebc1a93cb280e80358f7b1a6b5e985
jose <espitiaromerojosemanuel91gmail.com>
2026-05-27 10:54:32 -0500

    Fix: correccion de campos nulos y flexibilizacion de telefonos en formularios

--- apps/historial/models.py
+++ apps/historial/models.py
@@ -15,7 +15,7 @@ class Historial(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Usuario")
     accion = models.CharField(max_length=20, choices=ACCIONES, verbose_name="Acción")
     modulo = models.CharField(max_length=50, verbose_name="Módulo/Elemento")
-    elemento_id = models.CharField(max_length=50, blank=True, verbose_name="ID del Elemento")
+    elemento_id = models.CharField(max_length=50, blank=True, null=True, verbose_name="ID del Elemento")
     descripcion = models.TextField(verbose_name="Descripción detallada")
     fecha_hora = models.DateTimeField(auto_now_add=True, verbose_name="Fecha y Hora")
     ip_address = models.GenericIPAddressField(null=True, blank=True, verbose_name="Dirección IP")
