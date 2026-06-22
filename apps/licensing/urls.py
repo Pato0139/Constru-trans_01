0694dc28115e2a1e13c3d96fd5c1644fc55c2e6f
Pato0139 <edwardf5432@gmail.com>
2026-06-22 08:41:28 -0500

    fix: agregar urls.py a licensing

--- apps/licensing/urls.py
+++ apps/licensing/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from . import views
 
 app_name = "licensing"
-
 urlpatterns = [
-    path("expired/", views.license_expired, name="license_expired"),
-    path("activate/", views.license_activate, name="license_activate"),
+    path("expired/", views.license_expired, name="expired"),
+    path("activate/", views.license_activate, name="activate"),
 ]
