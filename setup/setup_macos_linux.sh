03b55ee211b7113694151d2c22afd26b4559ad12
Pato0139 <edwardf5432@gmail.com>
2026-07-09 10:50:39 -0500

    fix: Mejorar modo alto contraste, bloquear edición de documento y agregar comandos de poblamiento

--- setup/setup_macos_linux.sh
+++ setup/setup_macos_linux.sh
@@ -4,9 +4,11 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
 CYAN='\033[0;36m'
 MAGENTA='\033[0;35m'
 GRAY='\033[0;37m'
+WHITE='\033[1;37m'
 NC='\033[0m'
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
